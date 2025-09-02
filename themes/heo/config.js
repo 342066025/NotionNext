32e218471c089099927a4c027c1c6151c5718fec
342066025 <lijun342066025@gmail.com>
2025-09-02 18:07:59 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -81,28 +81,20 @@ const CONFIG = {
       color_2: '#ffffff'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+      title_1: 'matlab',
+      img_1: '/images/heo/matlab.png',
+      color_1: '#1f1f1f',
+      title_2: 'GPT',
+      img_2: '/images/heo/gpt.jpg',
       color_2: '#8f55ba'
     },
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
+      title_1: 'zetero',
+      img_1: '/images/heo/zotero_256x256x32.png',
       color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      title_2: 'overleaf',
+      img_2: '/images/heo/overleaf-icon-filled-256.png',
       color_2: '#2c51db'
-    },
-    {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#e9572b'
     },
     {
       title_1: 'Git',
